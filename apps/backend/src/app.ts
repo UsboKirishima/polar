dbf985b0805f5576398db78772d20c1af78eca3f
UsboKirishima <usbertibox@gmail.com>
2025-09-21 20:16:02 +0200

    feat(backend): add rate limit setup to app.ts

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -7,18 +7,32 @@ import type MessageResponse from "./interfaces/message-response.js";
 
 import api from "./routes/index.js";
 import * as middlewares from "./middlewares.js";
+import rateLimit from "express-rate-limit";
 
 const app = express();
 
+/* Rate limiter setup */
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    limit: 100,
+    message: {
+        status: 429,
+        error: "Too many attempts. Try again later."
+    },
+    standardHeaders: true,
+    legacyHeaders: false,
+})
+
+app.use(limiter);
 app.use(morgan("dev"));
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
 app.get<object, MessageResponse>("/", (req, res) => {
-  res.json({
-    message: "🦄🌈✨👋🌎🌍🌏✨🌈🦄",
-  });
+    res.json({
+        message: "🦄🌈✨👋🌎🌍🌏✨🌈🦄",
+    });
 });
 
 app.use("/api/v1", api);
